d6f3c94088e178d769b2e837fa3d99c487ca371e
sky2244 <tk_soccer0305@yahoo.co.jp>
2021-05-26 08:45:32 +0100

    add sample code

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 #! /usr/bin/python3
 
-def main():
-    print('main')
+# %%
+import numpy as np
+from bokeh.io import show, output_notebook
+from bokeh.plotting import figure, show
 
-if __name__ =='__main__':
-    main()
+
+def get_data():
+    x = np.linspace(0, 4 * np.pi, 100)
+    y = np.sin(x)
+    return (x, y)
+
+
+def make_figure():
+    x, y = get_data()
+    TOOLS = "pan,wheel_zoom,box_zoom,reset,save,box_select"
+
+    p1 = figure(title="Legend Example sample", tools=TOOLS)
+    p1.circle(x, y, legend="sin(x)")
+    p1.circle(x, 2 * y, legend="2*sin(x)", color="orange")
+    p1.circle(x, 3 * y, legend="3*sin(x)", color="green")
+
+    return p1
+
+
+output_notebook()
+show(make_figure())
